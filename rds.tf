cb5240aa7bd64db8d3760ca5a4271650dbccd36c
Samuel Berston <samuelberston@gmail.com>
2024-09-14 14:04:56 -0700

    add postgresql RDS resource

--- rds.tf
+++ rds.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "rds_postgres_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.backend_ec2_sg.id]  # Allow from Node.js EC2 security group
+    security_groups = [aws_security_group.backend_ec2_sg.id] # Allow from Node.js EC2 security group
   }
 
   # Allow all outbound traffic from the RDS instance
@@ -27,7 +27,7 @@ resource "aws_security_group" "rds_postgres_sg" {
 
 # Subnet group for the RDS instance
 resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "rds-subnet-group"
+  name = "rds-subnet-group"
   subnet_ids = [
     aws_subnet.terraform_orchestration_private_subnet_1.id,
     aws_subnet.terraform_orchestration_private_subnet_2.id
@@ -35,5 +35,27 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 
   tags = {
     Name = "rds-subnet-group"
+  }
+}
+
+# PostgreSQL RDS instance
+resource "aws_db_instance" "postgres_rds" {
+  identifier             = "terraform-postgres-rds"
+  allocated_storage      = 20
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  db_name                   = "tf-postgres-db"
+  username               = "postgres"
+  password               = "" # Update the password in RDS console then update it through GitHub environment secrets
+  parameter_group_name   = "default.aurora-postgresql15"
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_postgres_sg.id]
+  skip_final_snapshot    = true # FOR TESTING - Skip final snapshot on delete (use with caution)
+
+  # Disable public access
+  publicly_accessible = false
+
+  tags = {
+    Name = "terraform-postgres-rds"
   }
 }
