ba2e02887b150da44e74d6cb4c9323a5ca53d4c0
Samuel Berston <samuelberston@gmail.com>
2024-09-16 21:26:02 -0700

    move CloudWatch IAM role/policy to the cloudwatch.tf file and specify resources

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -13,3 +13,49 @@ resource "aws_cloudwatch_log_stream" "app_log_stream" {
   name           = "app-log-stream"
   log_group_name = aws_cloudwatch_log_group.app_log_group.name
 }
+
+# IAM role for CloudTrail to send logs to CloudWatch
+resource "aws_iam_role" "cloudwatch_role" {
+  name = "cloudtrail-cloudwatch-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# IAM policy for CloudWatch to access CloudTrail logs
+resource "aws_iam_policy" "cloudwatch_policy" {
+  name = "cloudwatch-logs-access-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:PutLogEvents",
+          "logs:CreateLogStream",
+        ],
+        Resource : [
+          "arn:aws:logs:us-east-1:123456789012:log-group:/aws/cloudtrail/logs:*",
+          "arn:aws:logs:us-east-1:123456789012:log-group:/aws/cloudtrail/logs:log-stream:*"
+        ]
+      }
+    ]
+  })
+}
+
+# Attach CloudWatch policy to role 
+resource "aws_iam_role_policy_attachment" "attach_cloudwatch_policy" {
+  role       = "cloudwatch_role"
+  policy_arn = aws_iam_policy.cloudwatch_policy.arn
+}
